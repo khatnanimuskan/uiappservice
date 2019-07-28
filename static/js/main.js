63f1fd326675a07fde66c6c64440d83da73bd5e6
Vineet Kumar <vkbamania@gmail.com>
2019-07-29 04:27:50 +0530

    vrrr

--- static/js/main.js
+++ static/js/main.js
@@ -1,40 +1,32 @@
-$(document).ready(function(){
-    // $.ajax({
-    //   url: "https://newcelebal.azurewebsites.net/.auth/me",
-    //   success: success,
-    //   dataType: dataType
-    // });
-    $.get("https://newcelebal.azurewebsites.net/.auth/me", function(data) {
-        console.log(data);
-    //   $(".result").html(data);
-    //   alert("Load was performed.");
+$(document).ready(function() {
+  $.get("https://newcelebal.azurewebsites.net/.auth/me", function(data) {
+    console.log(data);
+    document.getElementById("username").innerHTML = data[0].user_claims[11].val;
+  });
+  $(".generate").click(function() {
+    $.ajax({
+      type: "get",
+      url: "/all/",
+      contentType: "application/json",
+      success: function(data) {
+        $("#generateuniqueid").val(data["output"]);
+      },
+      error: function() {
+        alert("error");
+      }
     });
-
-    $(".generate").click(function(){
-        $.ajax({
-            type: "get",
-            url: "/all/",
-            contentType: 'application/json',
-            success: function(data){
-                $('#generateuniqueid').val(data['output']);
-            },
-            error: function(){
-                alert("error");
-            }
-        });
-    });
-    $('#datafactorysources').on('change', function() {
+  });
+  $("#datafactorysources").on("change", function() {
     var a = ["BQ", "Oracle", "SAP", "SalesForce"];
-    if (a.indexOf(this.value) > -1)
-    {
-        a.splice(a.indexOf(this.value, 1));
+    if (a.indexOf(this.value) > -1) {
+      a.splice(a.indexOf(this.value, 1));
     }
-    for(var i = 0; i < a.length; i++)
-    {
-        $("#"+a[i]+"_show").find('input').val('');
-        $("#"+a[i]+"_show").hide();
+    for (var i = 0; i < a.length; i++) {
+      $("#" + a[i] + "_show")
+        .find("input")
+        .val("");
+      $("#" + a[i] + "_show").hide();
     }
-     $("#"+this.value+"_show").show();
-    });
-
+    $("#" + this.value + "_show").show();
+  });
 });
